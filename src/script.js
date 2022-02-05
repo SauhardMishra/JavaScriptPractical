74991c31eeeee6a9e7d879042aa1be911f4e1782
SauhardMishra <sauhardmishra@cedcoss.com>
2022-02-05 14:30:14 +0530

    Comparison with the Greater Than Operator

--- src/script.js
+++ src/script.js
@@ -1,10 +1,11 @@
-// Setup
-function testStrict(val) {
-  if (val === 7) {
-    // Change this line
-    return "Equal";
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
   }
-  return "Not Equal";
-}
 
-testStrict(10);
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
